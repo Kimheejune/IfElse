4d632673d85e65411553bd5a8bb43aaf2993d9d4
Kimheejune <qwqw0102@hanmail.net>
2014-10-02 16:40:51 +0900

    ghjgjedrgyyertg bw

--- sketch.js
+++ sketch.js
@@ -1,3 +1,10 @@
+var trailX = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var trailY = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+
+
+
+
 function setup()
  {
   createCanvas(600, 600);
@@ -12,11 +19,25 @@ function setup()
   ellipse(200,200,100,100);
 }
 
+trailX[0] = (mouseX + trailX[0] * 4) / 5;
+trailY[0] = (mouseY + trailY[0] * 4) / 5;
+ellipse(trailX[0], trailY[0], 80, 80);
+
+for (var i = 1; i < 10; i = i + 1)
+{
+ trailX[i] = (trailX[i-1] + trailX[i] * 4) / 5;
+ trailY[i] = (trailY[i-1] + trailY[i] * 4) / 5;
+ ellipse(trailX[i], trailY[i], 80, 80); 
+}
+
+
+
+
 // draw function
 if (mouseX < 300) {
-  ellipse(200, 200, 100, 100);
+  ellipse(200, 200, 200, 200);
 } else {
-  ellipse(400, 200, 100, 100);
+  ellipse(400, 200, 400, 400);
 }
 
 // draw function
